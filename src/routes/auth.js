6ff91033eaa90cc06e5dfda1eb4e19684c17e382
Guilherme Gama <guilhermegamabs@gmail.com>
2022-10-31 19:23:56 -0300

    Hashing Passwords

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const User = require('../database/schemas/User');
 const router = Router();
-
+const { hashPassword } = require('../utils/helper');
 router.post('/login', (req, res) => {
   const { username, password } = req.body;
   if(username && password) {
@@ -17,11 +17,13 @@ router.post('/login', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-  const { username, password, email } = req.body;
+  const { username, email } = req.body;
   const userDB = await User.findOne({ $or: [{ username }, { email }]});
   if(userDB) {
     res.status(400).send({ msg: 'User already exists.' });
   } else {
+    const password = hashPassword(req.body.password);
+    console.log(password);
     const newUser = await User.create({ username, password, email});
     res.status(201);
   }
